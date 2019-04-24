14db112a19af43ffcc4971196d7935b64d36030c
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-04-23 22:42:04 -0400

    feat: Bottom navigation with empty DeckList

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { purple } from "./utils/colors";
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
+import { purple, white } from "./utils/colors";
 import { Constants } from "expo";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import DeckList from "./components/DeckList";
 
 function CustomStatusBar ({ backgroundColor, ...props }) {
   return (
@@ -11,12 +14,43 @@ function CustomStatusBar ({ backgroundColor, ...props }) {
   )
 }
 
+const Tabs = createBottomTabNavigator({
+  Decks: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards-outline' size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+// Required by React Navigation v3
+const MainNavigatorContainer = createAppContainer(Tabs)
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
         <CustomStatusBar backgroundColor={purple} barStyle='light-content' />
-        <Text>Mobile Flashcards</Text>
+        <MainNavigatorContainer />
       </View>
     );
   }
