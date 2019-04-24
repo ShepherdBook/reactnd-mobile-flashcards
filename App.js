4ffe36b87f498121b9fabe8fad3cf78959ec8efd
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-04-23 22:28:23 -0400

    feat: Status bar

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { purple } from "./utils/colors";
+import { Constants } from "expo";
+
+function CustomStatusBar ({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{flex: 1}}>
+        <CustomStatusBar backgroundColor={purple} barStyle='light-content' />
+        <Text>Mobile Flashcards</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
